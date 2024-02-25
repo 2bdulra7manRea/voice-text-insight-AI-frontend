23e2989aa10553ec97dd9dcc0155230851bd24fb
2bdulra7manRea <abdomosa987@gmail.com>
2024-02-25 16:36:17 +0200

    added functionality of text image

--- src/app/image-to-text/page.tsx
+++ src/app/image-to-text/page.tsx
@@ -1,14 +1,11 @@
+"use client"
 import FormTextImage from "@/components/image-text-form";
 
 
 export default function PageTextImage(){
 
 return <>
-
-<h3>Convert Image hand written to Text</h3>
-
 <FormTextImage/>
-
 </>
 
 }
